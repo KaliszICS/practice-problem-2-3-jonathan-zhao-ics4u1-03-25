8bbf1880315b8204e0ea3a8e7cc117e0403c3d99
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-03-20 19:02:33 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,24 @@
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static void bubbleSortString(String[] str) {
+		boolean swapped = true;
+		String temp = "";
+		for(int i = 0; i < str.length - 1 && swapped; i++ ) {
+			swapped = false;
+			for(int j = 0; j < str.length - 1 - i; j++) {
+				if(str[j].compareToIgnoreCase(str[j + 1]) > 0) {
+					temp = str[j];
+					str[j] = str[j + 1];
+					str[j + 1] = temp;
+					swapped = true;
+				}
+			}
+		}
 	}
 
 }
